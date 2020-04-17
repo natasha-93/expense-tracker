f4f5c83b4c9618fb1bb13a65f23848a253b4a050
Natasha Lawson <natasha.lawson93@gmail.com>
2020-04-17 15:49:08 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [newTransactionName, setNewTransactionName] = useState("");
+  const [newTransactionAmount, setNewTransactionAmount] = useState("");
+  const [transactionsHistory, setTransactionsHistory] = useState([
+    {
+      name: "Cash",
+      amount: 500,
+    },
+  ]);
+
+  const income = 2000;
+  const expenseTotal = transactionsHistory.reduce((total, { amount }) => {
+    return total + Number(amount);
+  }, 0);
+
   return (
-    <div className="app">
-      <h1>Hello World!</h1>
+    <div>
+      <h1>Expense Tracker</h1>
+      <p>Your Balance: ${income - expenseTotal}</p>
+      <p style={{ color: "green" }}>Income: ${income} </p>
+      <p style={{ color: "red" }}>Expenses: ${expenseTotal}</p>
+      <p>History:</p>
+      <ul>
+        {transactionsHistory.map((item, index) => {
+          return (
+            <li key={index}>
+              {item.name} - ${item.amount}
+            </li>
+          );
+        })}
+      </ul>
+      <p>Add New Transaction:</p>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setTransactionsHistory([
+            ...transactionsHistory,
+            { name: newTransactionName, amount: newTransactionAmount },
+          ]);
+          setNewTransactionName("");
+          setNewTransactionAmount("");
+        }}
+      >
+        <input
+          required
+          placeholder="Name"
+          value={newTransactionName}
+          onChange={(e) => {
+            setNewTransactionName(e.target.value);
+          }}
+        />
+        <input
+          required
+          type="number"
+          placeholder="Amount (+ / -)"
+          value={newTransactionAmount}
+          onChange={(e) => {
+            setNewTransactionAmount(e.target.value);
+          }}
+        />
+        <button>Add Transaction</button>
+      </form>
     </div>
   );
 }
