015046c2d5fb8083e9e018e1363574453057883c
Natasha Lawson <natasha.lawson93@gmail.com>
2020-04-20 11:42:54 -0600

    add select all checkbox

--- src/App.js
+++ src/App.js
@@ -7,25 +7,39 @@ function App() {
     {
       name: "Cash",
       amount: 500,
+      selected: false,
     },
   ]);
 
-  const income = 2000;
   const expenseTotal = transactionsHistory.reduce((total, { amount }) => {
-    return total + Number(amount);
+    const a = Number(amount);
+    return a < 0 ? total + a : total;
+  }, 0);
+  const incomeTotal = transactionsHistory.reduce((total, { amount }) => {
+    const a = Number(amount);
+    return a > 0 ? total + a : total;
   }, 0);
 
   return (
     <div>
       <h1>Expense Tracker</h1>
-      <p>Your Balance: ${income - expenseTotal}</p>
-      <p style={{ color: "green" }}>Income: ${income} </p>
+      <p>Your Balance: ${incomeTotal + expenseTotal}</p>
+      <p style={{ color: "green" }}>Income: ${incomeTotal} </p>
       <p style={{ color: "red" }}>Expenses: ${expenseTotal}</p>
       <p>History:</p>
       <ul>
         {transactionsHistory.map((item, index) => {
           return (
             <li key={index}>
+              <input
+                type="checkbox"
+                checked={item.selected}
+                onChange={(e) => {
+                  const newHistory = [...transactionsHistory];
+                  newHistory[index].selected = !newHistory[index].selected;
+                  setTransactionsHistory(newHistory);
+                }}
+              />
               {item.name} - ${item.amount}
             </li>
           );
@@ -37,7 +51,11 @@ function App() {
           e.preventDefault();
           setTransactionsHistory([
             ...transactionsHistory,
-            { name: newTransactionName, amount: newTransactionAmount },
+            {
+              name: newTransactionName,
+              amount: newTransactionAmount,
+              selected: false,
+            },
           ]);
           setNewTransactionName("");
           setNewTransactionAmount("");
@@ -61,6 +79,30 @@ function App() {
           }}
         />
         <button>Add Transaction</button>
+        <button
+          type="button"
+          onClick={(e) => {
+            const newHistory = transactionsHistory.filter((item) => {
+              return !item.selected;
+            });
+            setTransactionsHistory(newHistory);
+          }}
+        >
+          Delete Selected
+        </button>
+
+        <input
+          id="select-all"
+          type="checkbox"
+          onChange={(e) => {
+            console.log(e.target.checked);
+            const newHistory = transactionsHistory.map((item, index) => {
+              return { ...item, selected: e.target.checked };
+            });
+            setTransactionsHistory(newHistory);
+          }}
+        />
+        <label htmlFor="select-all">Select All</label>
       </form>
     </div>
   );
